d2498dae2bd6f2e8f70971358eae96de314fb482
Abe Choe <abechoe@thisisartium.com>
2023-09-20 14:37:42 -0700

    implement direct use of a v9 query
    Signed-off-by: Kevin Fitzpatrick <kevinfitzpatrick@thisisartium.com>

--- src/components/ComponentWithQueryClient.tsx
+++ src/components/ComponentWithQueryClient.tsx
@@ -1,11 +1,12 @@
 import { useQueryClient } from 'react-query';
 import { trpc } from '../utils/trpcHooks'
 
+// TODO: make invalidating queries work with queryClient
 const ComponentWithQueryClient = () => {
     const queryClient = useQueryClient();
     trpc.useQuery(['sofa'], {
       onSuccess() {
-        queryClient.invalidateQueries(['hello']);
+        // queryClient.invalidateQueries(['hello']);
       },
     });
     return <div>
--- src/components/DirectComponent.tsx
+++ src/components/DirectComponent.tsx
@@ -3,21 +3,37 @@ import { useEffect, useState } from "react";
 
 const DirectComponent = () => {
     // what we think it should be, according the docs:
-    const [greeting, setGreeting] = useState('')
+    const [greeting, setGreeting] = useState('');
+    const [hello, setHello] = useState('');
+
+    // TODO: direct v9 queries
 
     useEffect(() => {
         const fetchGreeting = async () => {
           const result = await directTrpcClient.greeting.query();
+          console.log(result)
           setGreeting(result.json);
         }
 
-        fetchGreeting();
+        const fetchHello = async () => {
+          const result = await directTrpcClient.hello.query({text: 'say hi'});
+          console.log({result})
+          setHello(result.json.greeting);
+        }
+
+        fetchGreeting().then(d => console.info({d})).catch(e => console.error({e}));
+        fetchHello().then(d => console.info({d})).catch(e => console.error({e}));
     }, []);
     
     return (
-    <div>
-      Sofa! {greeting}
-    </div>
+      <>
+      <div>
+        Sofa! {greeting}
+      </div>
+      <div>
+        Couch! {hello}
+      </div>
+      </>
     )
 }
     
